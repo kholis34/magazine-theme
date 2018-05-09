6a2edaed7eca1694d5843251b0581ea48eac82bc
Kholis Muhaimin <panghaidar@gmail.com>
2018-05-09 22:19:03 +0700

    Update widget-support.php

--- library/widget-support.php
+++ library/widget-support.php
@@ -14,27 +14,27 @@ add_action('widgets_init', 'theme_widgets_init');
 function kholisnet_widgets() {
 // Area 1, located in the footer. Empty by default.
 register_sidebar( array(
-'name' => __( 'First Footer Widget Area', 'pixelicons' ),
+'name' => __( 'First Footer Widget Area', 'kholisnet' ),
 'id' => 'first-footer-widget-area',
-'description' => __( 'The first footer widget area', 'pixelicons' ),
+'description' => __( 'The first footer widget area', 'kholisnet' ),
 'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
 'after_widget' => '</li>',
 'before_title' => '<h3 class="widget-title">',
 'after_title' => '</h3>',
 ) );
 register_sidebar( array(
-'name' => __( 'Second Footer Widget Area', 'pixelicons' ),
+'name' => __( 'Second Footer Widget Area', 'kholisnet' ),
 'id' => 'second-footer-widget-area',
-'description' => __( 'The Second footer widget area', 'pixelicons' ),
+'description' => __( 'The Second footer widget area', 'kholisnet' ),
 'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
 'after_widget' => '</li>',
 'before_title' => '<h3 class="widget-title">',
 'after_title' => '</h3>',
 ) );
 register_sidebar( array(
-'name' => __( 'Third Footer Widget Area', 'pixelicons' ),
+'name' => __( 'Third Footer Widget Area', 'kholisnet' ),
 'id' => 'third-footer-widget-area',
-'description' => __( 'The Third footer widget area', 'pixelicons' ),
+'description' => __( 'The Third footer widget area', 'kholisnet' ),
 'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
 'after_widget' => '</li>',
 'before_title' => '<h3 class="widget-title">',
